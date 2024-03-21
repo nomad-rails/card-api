8005f131d36962a9db4b5885f48d445cb8d1af18
Rapheal (jsxclan) <nhuxhr@gmail.com>
2024-03-21 15:42:13 +0100

    feat: config prisma ✨

---
+++ prisma/migrations/20240321144009_init/migration.sql
@@ -0,0 +1,62 @@
+-- CreateEnum
+CREATE TYPE "SessionStatusEnum" AS ENUM ('ACTIVE', 'EXPIRED', 'REVOKED');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "createAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updateAt" TIMESTAMP(3) NOT NULL,
+    "address" TEXT NOT NULL,
+    "holderId" TEXT,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "cards" (
+    "id" TEXT NOT NULL,
+    "createAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updateAt" TIMESTAMP(3) NOT NULL,
+    "userId" TEXT NOT NULL,
+    "cardId" TEXT,
+
+    CONSTRAINT "cards_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "sessions" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "expiresAt" TIMESTAMP(3) NOT NULL,
+    "lastSeenAt" TIMESTAMP(3),
+    "token" TEXT NOT NULL,
+    "status" "SessionStatusEnum" NOT NULL DEFAULT 'ACTIVE',
+
+    CONSTRAINT "sessions_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_address_key" ON "users"("address");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_holderId_key" ON "users"("holderId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "cards_userId_key" ON "cards"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "cards_cardId_key" ON "cards"("cardId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "sessions_userId_key" ON "sessions"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "sessions_token_key" ON "sessions"("token");
+
+-- AddForeignKey
+ALTER TABLE "cards" ADD CONSTRAINT "cards_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "sessions" ADD CONSTRAINT "sessions_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
