56a45cadf73838c128ff4f137c3dace2d888b7b6
Rapheal (jsxclan) <nhuxhr@gmail.com>
2024-03-21 16:40:44 +0100

    feat: rest API module ✨

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { ApiModule } from './api/api.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), ApiModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
